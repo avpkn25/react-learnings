a311d97ec7596ec2f92558eef897397dfb489a24
praveenkumar <praveenkumarnaidu88@gmail.com>
2025-01-23 06:55:56 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -240,3 +240,185 @@
 // }
 
 // export default App;
+
+// STYLING CSS - 5 Ways
+
+// 1. Inline
+
+// function App () {
+//     return <h1 style={{ color:"black", backgroundColor:"#ccc" }} >Hello I'm React</h1>
+// }
+
+// export default App
+
+// 2. Internal
+
+// function App() {
+//   const s = {
+//     color: "white",
+//     backgroundColor: "teal",
+//     padding: "10px",
+//   };
+//   return (
+//     <h1
+//       style={{
+//         color: s.color,
+//         backgroundColor: s.backgroundColor,
+//         padding: s.padding,
+//       }}
+//     >
+//       Hello I'm React
+//     </h1>
+//   );
+// }
+
+// export default App;
+
+// 3. External
+
+// import './index.css'
+
+// function App () {
+//     return (
+//         <>
+//             <h1>Hello I'm React</h1>
+//         </>
+//     )
+// }
+
+// export default App
+
+// 4. Bootstrap
+// imported bootstrap in index.js
+
+// import Button from 'react-bootstrap/Button';
+
+// function OutlineTypesExample() {
+//   return (
+//     <>
+//       <Button variant="outline-primary">Primary</Button>
+//       <Button variant="outline-secondary">Secondary</Button>
+//       <Button variant="outline-success">Success</Button>
+//       <Button variant="outline-warning">Warning</Button>
+//       <Button variant="outline-danger">Danger</Button>
+//       <Button variant="outline-info">Info</Button>
+//       <Button variant="outline-light">Light</Button>
+//       <Button variant="outline-dark">Dark</Button>
+//     </>
+//   );
+// }
+
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+
+// function BasicExample() {
+//   return (
+//     <Navbar expand="lg" className="bg-body-tertiary">
+//       <Container>
+//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link href="#home">Home</Nav.Link>
+//             <Nav.Link href="#link">Link</Nav.Link>
+//             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.2">
+//                 Another action
+//               </NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#action/3.4">
+//                 Separated link
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
+// function App() {
+//     // return <OutlineTypesExample />
+//   return <BasicExample />;
+// }
+
+// export default App;
+
+// 5. Tailwind CSS
+
+// not working in old reactjs framework
+
+// REACT ICONS
+
+// import { BsCartCheckFill } from "react-icons/bs";
+// import { FaGithub } from "react-icons/fa";
+
+// function App () {
+//     return (
+//         <>
+//             <BsCartCheckFill />
+//             <FaGithub />
+//         </>
+//     )
+// }
+
+// export default App
+
+// EVENTS
+
+// const Button = () => {
+//   const handleClick = (a, b) => {
+//     alert(a + b);
+//   };
+
+//   return <button onClick={() => handleClick(2, 2)}>Click</button>;
+// };
+
+// const Copy = () => {
+//   const copyHandler = () => {
+//     alert("Stop Stealing my Content!!!!");
+//   };
+//   return (
+//     <>
+//       <p onCopy={copyHandler}>
+//         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+//         cupiditate dolores inventore deleniti debitis et quo asperiores saepe,
+//         nam sequi?
+//       </p>
+//     </>
+//   );
+// };
+
+// const Move = () => {
+//   const moveHandler = () => {
+//     alert("You Hovered Me");
+//     // console.log("You Hovered Me");
+//   };
+//   return (
+//     <>
+//       <p onMouseMove={moveHandler}>
+//         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+//         cupiditate dolores inventore deleniti debitis et quo asperiores saepe,
+//         nam sequi?
+//       </p>
+//     </>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <>
+//       {/* <Button /> */}
+//       {/* <Copy /> */}
+//       <Move />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
